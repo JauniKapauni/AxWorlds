cde2c07d756fd84bc789543df75cf8f42d4c7c64
JauniKapauni <riches-goods.54@icloud.com>
2026-05-26 13:39:47 +0200

    feat: add TeleportCommand

--- src/main/java/de/jaunikapauni/axworlds/AxWorlds.java
+++ src/main/java/de/jaunikapauni/axworlds/AxWorlds.java
@@ -2,6 +2,7 @@ package de.jaunikapauni.axworlds;
 
 import de.jaunikapauni.axworlds.command.CreateCommand;
 import de.jaunikapauni.axworlds.command.ListCommand;
+import de.jaunikapauni.axworlds.command.TeleportCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxWorlds extends JavaPlugin {
@@ -11,6 +12,7 @@ public final class AxWorlds extends JavaPlugin {
         // Plugin startup logic
         getCommand("create").setExecutor(new CreateCommand());
         getCommand("list").setExecutor(new ListCommand());
+        getCommand("teleport").setExecutor(new TeleportCommand());
     }
 
     @Override
