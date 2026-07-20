c3b97a758fe010e9b667a0138ed94dbbb2c1f564
JauniKapauni <riches-goods.54@icloud.com>
2026-07-20 14:51:12 +0200

    fix(command): change delete to by executed async

--- src/main/java/de/jaunikapauni/axworlds/command/RemoveCommand.java
+++ src/main/java/de/jaunikapauni/axworlds/command/RemoveCommand.java
@@ -37,7 +37,9 @@ public class RemoveCommand implements CommandExecutor {
         }
         Bukkit.unloadWorld(world, true);
         File worldFolder = world.getWorldFolder();
-        deleteFolder(worldFolder);
+        Bukkit.getScheduler().runTaskAsynchronously(reference, () -> {
+            deleteFolder(worldFolder);
+        });
         sender.sendMessage("You have successfully removed " + args[0]);
         reference.getConfig().set("worlds" + "." + args[0], null);
         reference.saveConfig();
